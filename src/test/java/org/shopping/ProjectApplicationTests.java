61ccf30aa9dd1ffeb07b91018b108b126a8c9c15
BlueOceanDolphin <ehfrhfo313@naver.com>
2023-08-23 10:15:08 +0900

    Initial commit

---
+++ src/test/java/org/shopping/ProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package org.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	/* 테스트 */
+	@Test
+	void contextLoads() {
+
+	}
+}
